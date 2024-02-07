192480926e860e0addadb79a5d673b763fddde10
tzippykovalsky <150078127+tzippykovalsky@users.noreply.github.com>
2024-02-07 13:13:48 +0200

    Update product.js

--- routes/product.js
+++ routes/product.js
@@ -5,6 +5,8 @@ import { authAdmin } from "../middlewares/auth.js";
 
 const router = express.Router();
 
+
+router.get("/numPages", productController.getCountPages);
 router.get("/", productController.getAllProducts);
 router.get("/:id", productController.getProductById);
 router.post("/",authAdmin, productController.addPoduct);

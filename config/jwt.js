16ce4bbc921568febf224665e2e66c7d8aea2fab
tzippykovalsky <150078127+tzippykovalsky@users.noreply.github.com>
2024-02-21 22:46:22 +0200

    Update jwt.js

--- config/jwt.js
+++ config/jwt.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 
 export const generateToken=(user)=>{
     let token=jwt.sign(
-        {_id:user._id,userName:user.userName,role:user.role},process.env.JWT_SECRET,{expiresIn:"30m"}
+        {_id:user._id,userName:user.userName,role:user.role},process.env.JWT_SECRET,{expiresIn:"90m"}
     )
     return token;
 }

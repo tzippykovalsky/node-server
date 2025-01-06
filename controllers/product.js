31bc23c8d29c1f8e37ff2c96f4bc30bc2fe73afc
T.K. <150078127+tzippykovalsky@users.noreply.github.com>
2025-01-06 11:29:54 +0200

    Update product.js

--- controllers/product.js
+++ controllers/product.js
@@ -40,7 +40,7 @@ export const getCountPages = async (req, res) => {
         if (category !="ללא") {
             filterObject.category = category;
         }
-        let count = await Product.find(filterObject).count(); פונקציה הוצאה משימוש בגרסת מונגו 5+
+        let count = await Product.find(filterObject).count();// פונקציה הוצאה משימוש בגרסת מונגו 5+
         // let count = await Product.countDocuments(filterObject);במחשב עובד כאן לא למרות שעל אותו דטה לבדוק
         let numPages = count / itemsPerPage;
         res.json(numPages).status(200)
